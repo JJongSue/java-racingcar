7b29d6958bc9dea88be0dd01c9e823dd3fd8deef
jspark940321 <jspark940321@gmail.com>
2021-08-01 19:16:07 +0900

    feat: 자동차 이름변경 Main 적용

--- src/main/java/Car/RacingCarMain.java
+++ src/main/java/Car/RacingCarMain.java
@@ -6,13 +6,14 @@ import UI.OutputView;
 public class RacingCarMain {
     public static void main(String[] args) {
         InputView inputView = new InputView();
-        int carCount = inputView.getInt("자동차 대수는 몇 대 인가요?");
+        String[] carNames = inputView.getCarNames("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        int carCount = carNames.length;
         int gameCount = inputView.getInt("시도할 회수는 몇 회 인가요?");
 
-        RacingCar racingCar = new RacingCar(carCount);
+        RacingCar racingCar = new RacingCar(carCount, carNames);
         OutputView outputView = new OutputView();
         outputView.printResult(racingCar.getCars());
-        for (int i = 1; i < gameCount; i++) {
+        for (int i = 0; i < gameCount; i++) {
             racingCar.gameStart();
             outputView.printResult(racingCar.getCars());
         }
