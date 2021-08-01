9c65ec8177ba887026c48b65c8832e68d68f8b6f
jspark940321 <jspark940321@gmail.com>
2021-08-01 19:04:11 +0900

    test: RacingCar클래스 CarNames 추가에 의한 테스트 추가

--- src/test/java/Car/RacingCarTest.java
+++ src/test/java/Car/RacingCarTest.java
@@ -3,7 +3,6 @@ package Car;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-
 import static org.assertj.core.api.Assertions.*;
 
 class RacingCarTest {
@@ -11,16 +10,19 @@ class RacingCarTest {
 
     @BeforeEach
     void setUp() {
-        racingCar = new RacingCar(3);
+        String[] carNames = {"pobi", "crong", "honux"};
+        racingCar = new RacingCar(3, carNames);
     }
 
     @ParameterizedTest
     @CsvSource({
-            "3, 3",
-            "1, 1"
+            "3, 3, 'pobi,crong,honux'",
+            "1, 1, 'pobi'"
     })
-    void 자동차대수_테스트(int inputCarCount, int carCountAnswer) {
-        racingCar = new RacingCar(inputCarCount);
+    void 자동차대수_테스트(int inputCarCount, int carCountAnswer, String carNames) {
+        System.out.println(carNames);
+        String[] splitedCarName = carNames.split(",");
+        racingCar = new RacingCar(inputCarCount, splitedCarName);
         int carsSize = racingCar.getCars().size();
         assertThat(carsSize).isEqualTo(carCountAnswer);
     }
@@ -31,7 +33,8 @@ class RacingCarTest {
             "-1, 0보다 큰 값을 입력해주세요.",
     })
     void 자동차대수_테스트_예외발생(int carCount, String errMessage) throws IllegalAccessException {
-        assertThatThrownBy(() -> new RacingCar(carCount))
+        String[] carNames = {};
+        assertThatThrownBy(() -> new RacingCar(carCount, carNames))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(errMessage);
     }
